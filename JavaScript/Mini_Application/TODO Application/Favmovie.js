428b52466eb56cf2385b2e58104c4933768c106c
Raj Yadav <rajyadav968940@gmail.com>
2026-01-01 21:10:12 +0530

    Add files via upload

---
+++ JavaScript/Mini_Application/TODO Application/Favmovie.js
@@ -0,0 +1,9 @@
+const favMovie="Dhoom";
+
+let guess = prompt(" guess the favourite move ..");
+
+while((guess != favMovie) &&(guess !="quite"))
+{
+    guess=prompt("Wrong guess ,please try again");
+
+}
