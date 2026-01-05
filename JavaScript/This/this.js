91a66c4445f3c77410bf170471e21cf74e5373ab
Raj Yadav <rajyadav968940@gmail.com>
2026-01-05 23:23:38 +0530

    Add files via upload

---
+++ JavaScript/This/this.js
@@ -0,0 +1,17 @@
+const Student ={
+
+    Name : "Raj Yadav",
+    phy:56,
+    chem :87,
+
+    getSum()
+    {
+        let res =(this.phy +this.chem);
+
+        console.log(res);
+
+    }
+
+}
+
+Student.getSum();
